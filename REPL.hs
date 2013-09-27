6accbe20830a7ee06ba0b84b94cc4d98a754d464
Kata <lightquake@amateurtopologist.com>
2013-09-27 18:46:12 -0400

    Add a simple repl.

---
+++ REPL.hs
@@ -0,0 +1,26 @@
+module Main where
+
+import           Control.Applicative
+import           Evaluate
+import           Parser.AST
+import           Parser.RPN
+import           Pipes
+import qualified Pipes.Prelude       as P
+import           System.IO
+import           Text.Parsec.Error   (errorMessages, messageString)
+
+evaluatePrint :: Consumer String IO ()
+evaluatePrint = do
+    lift $ putStr "> " >> hFlush stdout
+    str <- await
+    lift $ case evaluate <$> parseRpn str of
+            Left err -> do
+                putStr "Error: "
+                mapM_ (putStrLn . messageString) $ errorMessages err
+            Right (IntLit i) -> print i
+            Right (DoubleLit d) -> print d
+            Right ast -> print ast
+    evaluatePrint
+
+main :: IO ()
+main = runEffect $ P.stdinLn >-> evaluatePrint
